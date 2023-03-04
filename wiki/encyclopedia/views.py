d67e97ca7e4d8eddf9c0bca7a9d365771542323e
chriscodes01 <chriscodes.business@gmail.com>
2023-03-03 22:03:56 -0600

    created entry template, implemented search bar

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+    # Search
+    if request.method == "POST":
+        entry = util.get_entry(request.POST.get("q"))
+        return render(request, "encyclopedia/entry.html", {"entry" : entry})
+        
+    # Home Page
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
     })
 
-def viewEntry(request, entry):
-    return render(request, "encyclopedia/viewEntry.html", {
-
-    })
+#def entry(request, entry):
+    #return render(request, "encyclopedia/entry.html", {
+        #"entry": util.get_entry(entry)
+    #})
 
--- wiki/wiki/urls.py
+++ wiki/wiki/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-app_name = "encyclopedia"
+# app_name = "encyclopedia"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("encyclopedia.urls"))
