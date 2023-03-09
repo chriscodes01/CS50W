08456137bf3b68cddb6f98d814515f80137ee5d8
chriscodes01 <chriscodes.business@gmail.com>
2023-03-08 22:04:45 -0600

    changed how rendering entries work, created new path

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,5 @@ from . import views
 # app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    #path("entry", views.entry, name="entry")
+    path("wiki/<str:entry>", views.entry, name="entry")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -5,19 +5,12 @@ from . import util
 
 
 def index(request):
-    # Search
-    if request.method == "POST":
-        entry = util.get_entry(request.POST.get("q"))
-        return render(request, "encyclopedia/entry.html", {"entry" : entry})
-        
-    # Home Page
-    else:
-        return render(request, "encyclopedia/index.html", {
-            "entries": util.list_entries()
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
     })
 
-#def entry(request, entry):
-    #return render(request, "encyclopedia/entry.html", {
-        #"entry": util.get_entry(entry)
-    #})
+def entry(request, entry):
+    return render(request, "encyclopedia/entry.html", {
+        "entry": util.get_entry(entry)
+    })
 
