be3f2fdda670fcf80f8417ed71d8c5caba528143
chriscodes01 <chriscodes.business@gmail.com>
2023-03-14 19:21:08 -0500

    enabled direct links to entries from index, added search/create request

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 # app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:entry>", views.entry, name="entry")
+    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
+    path("create/", views.create, name="create")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -12,15 +12,15 @@ def index(request):
 def mdToHtml(entry):
     title = util.get_entry(entry)
     markdowner = Markdown()
+
     if title == None:
         return None
     else:
         return markdowner.convert(title)
 
-
-
 def entry(request, entry):
     content = mdToHtml(entry)
+
     if content == None:
         return render(request, "encyclopedia/error.html")
     else:
@@ -29,3 +29,25 @@ def entry(request, entry):
             "content": content
     })
 
+def search(request):
+    if request.method == "POST":
+        query = request.POST["q"]
+        content = mdToHtml(query)
+
+        if content == True:
+            return render(request, "encyclopedia/entry.html", {
+                "entry": query,
+                "content": content
+            })
+        else:
+            database = util.list_entries()
+            results = []
+            for entry in database:
+                if query.lower() in entry.lower():
+                    results.append(entry)
+            return render(request, "encyclopedia/search.html", {
+                "results": results
+            })
+
+def create(request):
+    return render(request, "encyclopedia/create.html")
