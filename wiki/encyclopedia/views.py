210c81c89bc7d36f5e8d38c5e02bc5aace5ee27e
chriscodes01 <chriscodes.business@gmail.com>
2023-03-15 17:12:54 -0500

    finished site

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from markdown2 import Markdown
+import random
 
 from . import util
 
@@ -22,7 +23,9 @@ def entry(request, entry):
     content = mdToHtml(entry)
 
     if content == None:
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html", {
+            "message": "This entry does not exist."
+        })
     else:
         return render(request, "encyclopedia/entry.html", {
             "entry": entry,
@@ -50,4 +53,64 @@ def search(request):
             })
 
 def create(request):
-    return render(request, "encyclopedia/create.html")
+    if request.method == "POST":
+        title = request.POST["title"]
+        checkEntry = util.get_entry(title)
+
+        if checkEntry == None:
+            entryContent = request.POST["content"]
+            util.save_entry(title, entryContent)
+            content = mdToHtml(title)
+
+            print(title)
+            print(content)
+            return render(request, "encyclopedia/entry.html", {
+                "entry": title,
+                "content": content
+            })
+        
+        else:
+            return render(request, "encyclopedia/error.html", {
+                "message": "This entry already exists."
+            })
+    else:
+        return render(request, "encyclopedia/create.html")
+    
+def edit(request):
+    if request.method == "POST":
+        entry = request.POST["entry"]
+        content = util.get_entry(entry)
+        return render(request, "encyclopedia/edit.html", {
+            "entry": entry,
+            "content": content
+        })
+    
+    else:
+        return render(request, "encyclopedia/index.html", {
+
+        })
+    
+def save(request):
+    if request.method == "POST":
+        entry = request.POST["entry"]
+        content = request.POST["content"]
+
+        util.save_entry(entry, content)
+        content = mdToHtml(entry)
+
+        return render(request, "encyclopedia/entry.html", {
+            "entry": entry,
+            "content": content
+        })
+    else:
+        return render(request, "encyclopedia/index.html")
+
+def rand(request):
+    entries = util.list_entries()
+    rand = random.choice(entries)
+    content = mdToHtml(rand)
+
+    return render(request, "encyclopedia/entry.html", {
+        "entry": rand,
+        "content": content
+    })
